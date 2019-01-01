6e3f45a4f451a23d22df65fb2933a15805d6706c
JAA1998 <40919117+JAA1998@users.noreply.github.com>
2019-01-01 17:59:39 -0400

    Update Respuesta.cs

--- Sorteos/Sorteos/Respuesta.cs
+++ Sorteos/Sorteos/Respuesta.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
@@ -9,15 +9,8 @@ namespace Sorteos
     [DataContract]
     public class Respuesta
     {
-
         [DataMember]
         public string Mensaje { get; set; }
-
-        /*[DataMember]
-        public Sorteo S { get; set; }
-
-        [DataMember]
-        public string Estatus { get; set; }*/
 
         public Respuesta(string msg)
         {
