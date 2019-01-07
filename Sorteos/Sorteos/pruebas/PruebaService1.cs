c252ed7b92e072678e2e0a9653481e129a6c6954
OscarM <oscarmarquez2218@gmail.com>
2019-01-07 10:44:56 -0500

    Cambios en las pruebas

--- Sorteos/Sorteos/pruebas/PruebaService1.cs
+++ Sorteos/Sorteos/pruebas/PruebaService1.cs
@@ -13,8 +13,8 @@ namespace pruebas
             sorteo1.ID_SORTEO=1;
             sorteo1.ID_JUEGO=2;
             sorteo1.ID_ITEM=1;
-            sorteo1.HORA= 5:30;
-            sorteo1.DIA=12;
+            sorteo1.HORA= "5:30";
+            sorteo1.DIA="Lunes";
             try
             {
                 Service1 result= new Service1();
@@ -66,8 +66,8 @@ namespace pruebas
             sorteo1.ID_SORTEO=1;
             sorteo1.ID_JUEGO=2;
             sorteo1.ID_ITEM=1;
-            sorteo1.HORA= 5:30;
-            sorteo1.DIA=12;
+            sorteo1.HORA= "3:00";
+            sorteo1.DIA="Viernes";
             
             try
             {
@@ -101,6 +101,174 @@ namespace pruebas
 
                 Assert.AreNotEqual(expected, result.ConsultarSorteoxJuego(sorteo1));
                 
+            }
+            catch (Exception e)
+            {
+                
+                return new Respuesta("Error: " + e.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+         [Theory]
+        [InlineData(2)]
+        public void PruebaConsultarJuego(int idJuego)
+        {
+            Consultas result = new Consultas();
+
+            try
+            {
+                
+                Assert.Equals(1,result.ConsultarJuego(idJuego));
+            }
+            catch (Exception e)
+            {
+                
+                return new Respuesta("Error: " + e.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+        [Theory]
+        [InlineData(3)]
+        public void PruebaConsultarItem(int idItem)
+        {
+            Consultas result = new Consultas();
+
+            try
+            {
+                Assert.Equals(1,result.ConsultarItem(idItem));
+            }
+            catch (Exception e)
+            {
+                
+                return new Respuesta("Error: " + e.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        [Theory]
+        [InlineData(1)]
+        public void PruebaConsultarSorteo(int idSorteo)
+        {
+            Consultas result = new Consultas();
+
+            try
+            {
+                Assert.Equals(1,result.ConsultarItem(idSorteo));
+            }
+            catch (Exception e)
+            {
+                
+                return new Respuesta("Error: " + e.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        [Theory]
+        [InlineData(1,4)]
+        public void PruebaConsultarSJ(int idSorteo, int idJuego)
+        {
+            Consultas result = new Consultas();
+
+            try
+            {
+                Assert.Equals(1,result.ConsultarSJ(idSorteo, idSorteo));
+            }
+            catch (Exception e)
+            {
+                
+                return new Respuesta("Error: " + e.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        [Theory]
+        [InlineData(1,"Martes")]
+        public void PruebaConsultarDia(int idSorteo, string dia)
+        {
+            Consultas result = new Consultas();
+
+            try
+            {
+                Assert.Equals(0,result.ConsultarDia(idSorteo, dia));
+            }
+            catch (Exception e)
+            {
+                
+                return new Respuesta("Error: " + e.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        [Theory]
+        [InlineData(1,"Martes", "4:30")]
+        public void PruebaConsultarHora(int idSorteo, string dia, string hora)
+        {
+            Consultas result = new Consultas();
+
+            try
+            {
+                Assert.Equals(0,result.ConsultarHora(idSorteo, hora, dia));
+            }
+            catch (Exception e)
+            {
+                
+                return new Respuesta("Error: " + e.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        [Theory]
+        [InlineData(4)]
+        public void PruebaConsultarApuestas(int idSorteo)
+        {
+            Consultas result = new Consultas();
+
+            try
+            {
+                
+                Assert.Equals(1,result.ConsultarApuestas(idSorteo));
+            }
+            catch (Exception e)
+            {
+                
+                return new Respuesta("Error: " + e.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+// Nose si esta prueba es asi por ser un void
+        [Theory]
+        [InlineData(1, 4, 200)]
+        public void PruebaConsultarDatosItem(int idItem, ref int cupo, ref float monto)
+        {
+            Consultas result = new Consultas();
+
+            try
+            {
+                Assert.Equals(0,result.ConsultarDatosItem(idItem, cupo, monto));
             }
             catch (Exception e)
             {
